3454e5a5b95f83cf5adafd19da9df578747d117e
o4 <o@o.o>
2016-05-12 18:27:41 +0200

    Change Crawler Interface to included sendDataToBackend() method

--- src/main/java/de/orfap/fap/crawler/crawler/Crawler.java
+++ src/main/java/de/orfap/fap/crawler/crawler/Crawler.java
@@ -10,42 +10,47 @@ import org.springframework.hateoas.Resource;
  */
 public interface Crawler {
     /**
-     * Writes the airlines into backend
+     * Crawlsings the airlines, as in crawling & parsing.
      * @param urlToRead where to read
      * @throws Exception
      */
     void getAirlines(String urlToRead) throws Exception;
 
     /**
-     * Writes the markets into backend
+     * Crawlsing the markets, as in crawling & parsing.
      * @param urlToRead where to read
      * @throws Exception
      */
     void getMarkets(String urlToRead) throws Exception;
 
     /**
-     * Writes the routes into backend
+     * Crawlsing the routes, as in crawling & parsing.
      * @param urlToRead where to read
      * @throws Exception
      */
     void getRoutes(String urlToRead) throws Exception;
 
     /**
-     * Sends an airline to the backend
+     * Sends the crawsinged (as in crawled & parsed) airlines, markets & routes to the backend.
+     * Airlines & markets not mentioned in the routes are not sent to the backend.
+     */
+    public void sendDataToBackend();
+    /**
+     * Sends an airline to the backend.
      * @param airline
      * @return
      */
     Resource<Airline> sendAirlineToBackend(Airline airline);
 
     /**
-     * Sends a market to the backend
+     * Sends a market to the backend.
      * @param market
      * @return
      */
     Resource<Market> sendMarketToBackend(Market market);
 
     /**
-     * Sends a route to the backend
+     * Sends a route to the backend.
      * @param route
      * @return
      */
