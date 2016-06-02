6da07d1728ac528f975ea9892486869fe73fd15b
o4 <o@o.o>
2016-06-02 17:48:16 +0200

    issue17: only get databasecolumns needed don't change functionality

--- src/main/java/de/orfap/fap/crawler/controller/CrawlerController.java
+++ src/main/java/de/orfap/fap/crawler/controller/CrawlerController.java
@@ -30,6 +30,6 @@ public class CrawlerController {
         crawler.getMarkets("http://www.transtats.bts.gov/Download_Lookup.asp?Lookup=L_CITY_MARKET_ID");
         crawler.getRoutes("http://transtats.bts.gov/DownLoad_Table.asp?Table_ID=311&Has_Group=3&Is_Zipped=0", usedYear);
         crawler.getFlights("http://transtats.bts.gov/DownLoad_Table.asp?Table_ID=236&Has_Group=3&Is_Zipped=0", usedYear);
-//        crawler.sendDataToBackend();
+        crawler.sendDataToBackend();
     }
 }
