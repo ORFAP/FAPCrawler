0a6501af545ef31ce7fbbf06f5b842ed291ef47c
Arne <arne.schoentag@imagefiguren.de>
2016-06-27 15:34:10 +0200

    add further Tests

--- src/test/java/de/orfap/fap/crawler/ResourceBuilderTest.java
+++ src/test/java/de/orfap/fap/crawler/ResourceBuilderTest.java
@@ -2,14 +2,15 @@ package de.orfap.fap.crawler;
 
 import de.orfap.fap.crawler.crawlerpipes.AirlineBuilder;
 import de.orfap.fap.crawler.crawlerpipes.CsvFileStringExtractor;
+import de.orfap.fap.crawler.crawlerpipes.FlightBuilder;
 import de.orfap.fap.crawler.crawlerpipes.MarketBuilder;
 import de.orfap.fap.crawler.crawlerpipes.RouteBuilder;
 import de.orfap.fap.crawler.crawlerpipes.ZipFileStringExtractor;
 import de.orfap.fap.crawler.domain.Airline;
 import de.orfap.fap.crawler.domain.Market;
 import de.orfap.fap.crawler.domain.Route;
-import org.junit.Test;
 import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Created by Arne on 27.06.2016.
@@ -53,18 +54,47 @@ public class ResourceBuilderTest {
         String nextLine = extractor.deliver();
         Route nextRoute;
         while(nextLine != null){
-                nextRoute = routeBuilder.transform(nextLine);
-                Assert.assertNotNull(nextRoute);
-                Assert.assertNotNull(nextRoute.getAirline());
-                Assert.assertNotNull(nextRoute.getDestination());
-                Assert.assertNotEquals("", nextRoute.getDestination());
-                Assert.assertNotNull(nextRoute.getSource());
-                Assert.assertNotEquals("", nextRoute.getSource());
-                Assert.assertTrue(nextRoute.getCancelled() == 0.0 || nextRoute.getCancelled() == 1.0);
-                Assert.assertTrue(0.0 == nextRoute.getDelays());
+            nextRoute = routeBuilder.transform(nextLine);
+            Assert.assertNotNull(nextRoute);
+            Assert.assertNotNull(nextRoute.getAirline());
+            Assert.assertNotNull(nextRoute.getDestination());
+            Assert.assertNotEquals("", nextRoute.getDestination());
+            Assert.assertNotNull(nextRoute.getSource());
+            Assert.assertNotEquals("", nextRoute.getSource());
+            Assert.assertNotNull(nextRoute.getDate());
+            Assert.assertTrue(nextRoute.getCancelled() == 0.0 || nextRoute.getCancelled() == 1.0);
+            Assert.assertTrue(0.0 == nextRoute.getDelays());
+            Assert.assertTrue(0.0 == nextRoute.getFlightCount());
+            Assert.assertTrue(nextRoute.getPassengerCount() >= 0.0);
+            nextLine = extractor.deliver();
+        }
+    }
+
+    @Test
+    public void flightBuilderTest(){
+        ZipFileStringExtractor extractor = new ZipFileStringExtractor("src/test/resources/FlightsTest.zip");
+        FlightBuilder flightBuilder = new FlightBuilder(true, null);
+        String nextLine = extractor.deliver();
+        Route nextRoute;
+        while(nextLine != null){
+            nextRoute = flightBuilder.transform(nextLine);
+            Assert.assertNotNull(nextRoute);
+            Assert.assertNotNull(nextRoute.getAirline());
+            Assert.assertNotNull(nextRoute.getDestination());
+            Assert.assertNotEquals("", nextRoute.getDestination());
+            Assert.assertNotNull(nextRoute.getSource());
+            Assert.assertNotEquals("", nextRoute.getSource());
+            Assert.assertNotNull(nextRoute.getDate());
+            if (nextRoute.getCancelled() == 1.0){
                 Assert.assertTrue(0.0 == nextRoute.getFlightCount());
-                Assert.assertTrue(nextRoute.getPassengerCount() >= 0.0);
-                nextLine = extractor.deliver();
+            } else if (nextRoute.getCancelled() == 0.0) {
+                Assert.assertTrue(1.0 == nextRoute.getFlightCount());
+            } else {
+                Assert.fail("Route cancelled invalid value.");
+            }
+            Assert.assertTrue(nextRoute.getDelays() >= 0.0);
+            Assert.assertTrue(nextRoute.getPassengerCount() == 0.0);
+            nextLine = extractor.deliver();
         }
     }
 }
