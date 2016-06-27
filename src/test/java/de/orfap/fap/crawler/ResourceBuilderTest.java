f3e064686cba759ef4d35bf81ffe585b815b2161
Arne <arne.schoentag@imagefiguren.de>
2016-06-27 15:21:07 +0200

    add Tests

---
+++ src/test/java/de/orfap/fap/crawler/ResourceBuilderTest.java
@@ -0,0 +1,70 @@
+package de.orfap.fap.crawler;
+
+import de.orfap.fap.crawler.crawlerpipes.AirlineBuilder;
+import de.orfap.fap.crawler.crawlerpipes.CsvFileStringExtractor;
+import de.orfap.fap.crawler.crawlerpipes.MarketBuilder;
+import de.orfap.fap.crawler.crawlerpipes.RouteBuilder;
+import de.orfap.fap.crawler.crawlerpipes.ZipFileStringExtractor;
+import de.orfap.fap.crawler.domain.Airline;
+import de.orfap.fap.crawler.domain.Market;
+import de.orfap.fap.crawler.domain.Route;
+import org.junit.Test;
+import org.junit.Assert;
+
+/**
+ * Created by Arne on 27.06.2016.
+ */
+public class ResourceBuilderTest {
+
+    @Test
+    public void airlineBuilderTest(){
+        CsvFileStringExtractor extractor = new CsvFileStringExtractor("src/test/resources/AirlineTest.csv");
+        AirlineBuilder airlineBuilder = new AirlineBuilder(true, null);
+        String nextLine = extractor.deliver();
+        Airline nextAirline;
+        while(nextLine != null){
+            nextAirline = airlineBuilder.transform(nextLine);
+            Assert.assertNotNull(nextAirline);
+            Assert.assertNotNull(nextAirline.getId());
+            Assert.assertNotNull(nextAirline.getName());
+            nextLine = extractor.deliver();
+        }
+    }
+
+    @Test
+    public void marketBuilderTest(){
+        CsvFileStringExtractor extractor = new CsvFileStringExtractor("src/test/resources/MarketTest.csv");
+        MarketBuilder marketBuilder = new MarketBuilder(true, null);
+        String nextLine = extractor.deliver();
+        Market nextMarket;
+        while(nextLine != null){
+            nextMarket = marketBuilder.transform(nextLine);
+            Assert.assertNotNull(nextMarket);
+            Assert.assertNotNull(nextMarket.getId());
+            Assert.assertNotNull(nextMarket.getName());
+            nextLine = extractor.deliver();
+        }
+    }
+
+    @Test
+    public void routeBuilderTest(){
+        ZipFileStringExtractor extractor = new ZipFileStringExtractor("src/test/resources/RoutesTest.zip");
+        RouteBuilder routeBuilder = new RouteBuilder(true, null);
+        String nextLine = extractor.deliver();
+        Route nextRoute;
+        while(nextLine != null){
+                nextRoute = routeBuilder.transform(nextLine);
+                Assert.assertNotNull(nextRoute);
+                Assert.assertNotNull(nextRoute.getAirline());
+                Assert.assertNotNull(nextRoute.getDestination());
+                Assert.assertNotEquals("", nextRoute.getDestination());
+                Assert.assertNotNull(nextRoute.getSource());
+                Assert.assertNotEquals("", nextRoute.getSource());
+                Assert.assertTrue(nextRoute.getCancelled() == 0.0 || nextRoute.getCancelled() == 1.0);
+                Assert.assertTrue(0.0 == nextRoute.getDelays());
+                Assert.assertTrue(0.0 == nextRoute.getFlightCount());
+                Assert.assertTrue(nextRoute.getPassengerCount() >= 0.0);
+                nextLine = extractor.deliver();
+        }
+    }
+}
